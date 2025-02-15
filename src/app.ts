aad112015646edf871797d715e36922bf205c30c
Abduqodir65 <abduqodirgithub@gmail.com>
2025-02-15 14:07:55 +0500

    initialize project structure and configuration files

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity.{ts,js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+    }),
+
+    ServeStaticModule.forRoot({
+      serveRoot: "/uploads",
+      rootPath: "./uploads"
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
