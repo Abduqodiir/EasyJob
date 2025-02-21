814808f6943470dfdfab4284d798e183807ed9b6
Abduqodir65 <abduqodirgithub@gmail.com>
2025-02-21 11:48:56 +0500

    add Auth and Mail modules to app imports; include Token entity in TypeORM configuration

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { appConfig } from '@config';
-import { Application, ApplicationsModule, FileModule, Job, JobModule, Resume, ResumeModule, User, UserModule } from '@modules';
+import { Application, ApplicationsModule, AuthModule, FileModule, Job, JobModule, MailModule, Resume, ResumeModule, Token, User, UserModule} from '@modules';
 
 @Module({
   imports: [
@@ -27,14 +27,16 @@ import { Application, ApplicationsModule, FileModule, Job, JobModule, Resume, Re
         database: configService.get<string>('DB_NAME'),
         synchronize: true,
         autoLoadEntities: true,
-        entities: [User, Application, Resume, Job]
+        entities: [User, Application, Resume, Job, Token]
       }),
     }),
+    AuthModule,
     FileModule,
+    MailModule,
     UserModule,
     JobModule,
     ApplicationsModule,
-    ResumeModule
+    ResumeModule,
   ],
   controllers: [],
   providers: [],
